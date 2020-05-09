6dc6ea07001ebe81dc29696b34914fdfaebdf4ec
Vladyslav Symonenko <vladyslavsymonenko@Vladyslavs-MacBook-Pro.local>
2020-05-09 16:58:44 +0300

    add bot

---
+++ index.js
@@ -0,0 +1,34 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+let IaId = '';
+// Create a bot that uses 'polling' to fetch new updates
+console.log('args',process.argv[2]);
+
+const token = '1131189638:AAE9WKvFgxLvNlKHINzo25E7uJ_L_HOSdyg';
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+IaId = chatId;
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, '1');
+});
+const envs = {};
+// setInterval( ()=> IaId !== '' && bot.sendMessage(IaId, '1'),5000);
